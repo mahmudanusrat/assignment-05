75eb197540b825543c545d4c3be71e0e2296e6b6
mahmudanusrat <mnurat315@gmail.com>
2024-09-24 11:50:14 +0600

    navbar part

--- js/feature.js
+++ js/feature.js
@@ -1,13 +1,28 @@
 function showHistoryById(id) {
     document.getElementById("donation-cards").classList.add("hidden");
     document.getElementById('history-container').classList.add('hidden');
+
     document.getElementById(id).classList.remove('hidden');
   
   }
   
-document.getElementById('btn-donation').addEventListener('click', function(event){
-    showHistoryById('donation-cards')
+const btnDonation = document.getElementById('btn-donation');
+const btnHistory = document.getElementById('btn-history');
+
+btnDonation.addEventListener('click', function() {
+    showHistoryById('donation-cards');
+    toggleButtonColors(btnDonation, btnHistory);
 });
-document.getElementById('btn-history').addEventListener('click', function(event){
-    showHistoryById('history-container')
+
+btnHistory.addEventListener('click', function() {
+    showHistoryById('history-container');
+    toggleButtonColors(btnHistory, btnDonation);
 });
+
+function toggleButtonColors(activeBtn, inactiveBtn) {
+    activeBtn.classList.add('bg-[#B4F461]');
+    activeBtn.classList.remove('bg-white');
+
+    inactiveBtn.classList.remove('bg-[#B4F461]');
+    inactiveBtn.classList.add('bg-white');
+};
